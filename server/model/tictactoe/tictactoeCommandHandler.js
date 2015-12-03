4526ebc977100e3d6120700fac8955bff4b361bc
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2015-12-03 13:55:43 +0000

    should create game pass dummy

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,41 +1,13 @@
 module.exports = function tictactoeCommandHandler(events) {
-  var gameCreatedEvent = events[0];
-
-  var handlers = {
-    "CreateGame": function (cmd) {
-      {
-        return [{
-          id: cmd.id,
-          event: "GameCreated",
-          userName: cmd.userName,
-          timeStamp: cmd.timeStamp
-        }];
-      }
-    },
-    "JoinGame": function (cmd) {
-      {
-        if (gameCreatedEvent === undefined) {
-          return [{
-            id: cmd.id,
-            event: "GameDoesNotExist",
-            userName: cmd.userName,
-            timeStamp: cmd.timeStamp
-          }];
-        }
-        return [{
-          id: cmd.id,
-          event: "GameJoined",
-          userName: cmd.userName,
-          otherUserName: gameCreatedEvent.userName,
-          timeStamp: cmd.timeStamp
-        }];
-      }
-    }
-  };
-
   return {
-    executeCommand: function (cmd) {
-      return handlers[cmd.comm](cmd);
+    executeCommand: function (command) {
+      return [{
+        id: 1,
+        event: 'GameCreated',
+        gameID: 1,
+        userName: 'Kristjan',
+        timeStamp: '2015.12.03T13:17:10'
+      }];
     }
-  };
-};
+  }
+}
