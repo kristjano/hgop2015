d876d9272f8b91e7f329ec7559bb9dbcec1c3090
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2015-12-14 10:38:31 +0000

    Added git revision hash to docker build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,21 +2,44 @@
 
 # Add server-side testing to test reports
 export MOCHA_REPORTER=xunit
-export MOCHA_REPORT=server-tests.xml
+export MOCHA_REPORT=reports/mocha/server-tests.xml
 
 echo Cleaning...
 rm -rf ./dist
 
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
 echo Building app
 grunt
-# Abort the script if grunt fails
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Grunt build failed with exit code " $rc
     exit $rc
 fi
+
+cat > ./dist/githash.txt <<_EOF_
+$GIT_COMMIT
+_EOF_
+
+cat > ./dist/public/version.html << _EOF_
+<!doctype html>
+<head>
+   <title>TicTacToe version information</title>
+</head>
+<body>
+   <span>Origin:</span> <span>$GITHUB_URL</span>
+   <span>Revision:</span> <span>$GIT_COMMIT</span>
+   <p>
+   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
+</body>
+_EOF_
 
 cp ./Dockerfile ./dist/
 
@@ -29,9 +52,7 @@ if [[ $rc != 0 ]] ; then
 fi
 
 echo Building docker image
-docker build -t kristjano/tictactoe .
-# Abort the script if docker build fails
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+docker build -t kristjano/tictactoe:$GIT_COMMIT  .
 
 rc=$?
 if [[ $rc != 0 ]] ; then
@@ -41,7 +62,7 @@ fi
 
 echo " -- Pushing to docker hub"
 echo
-docker push kristjano/tictactoe
+docker push kristjano/tictactoe:$GIT_COMMIT 
 echo
 
 echo "Done"
